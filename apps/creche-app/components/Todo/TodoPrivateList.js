35011e2979ae728ae7218e591731586013ba630d
tomonakar <tomohisa.nak@gmail.com>
2020-08-17 15:43:16 +0900

    feat: add useQuery hook

--- apps/creche-app/components/Todo/TodoPrivateList.js
+++ apps/creche-app/components/Todo/TodoPrivateList.js
@@ -1,26 +1,24 @@
 import React, { useState, Fragment } from "react";
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks'
 
 import TodoItem from "./TodoItem";
 import TodoFilters from "./TodoFilters";
+
+const GET_MY_TODOS = gql`
+  query getMyTodos {
+    todos(where: {is_public: {_eq: false}}, order_by: {created_at: desc}) {
+      id
+      title
+      created_at
+      is_completed
+    }
+  }`
 
 const TodoPrivateList = props => {
   const [state, setState] = useState({
     filter: "all",
     clearInProgress: false,
-    todos: [
-      {
-        id: "1",
-        title: "This is private todo 1",
-        is_completed: true,
-        is_public: false
-      },
-      {
-        id: "2",
-        title: "This is private todo 2",
-        is_completed: false,
-        is_public: false
-      }
-    ]
   });
 
   const filterResults = filter => {
@@ -32,11 +30,13 @@ const TodoPrivateList = props => {
 
   const clearCompleted = () => {};
 
-  let filteredTodos = state.todos;
+  const { todos } = props
+
+  let filteredTodos = todos
   if (state.filter === "active") {
-    filteredTodos = state.todos.filter(todo => todo.is_completed !== true);
+    filteredTodos = todo.filter(todo => todo.is_completed !== true)
   } else if (state.filter === "completed") {
-    filteredTodos = state.todos.filter(todo => todo.is_completed === true);
+    filteredTodos = todos.filter(todo => todo.is_completed === true);
   }
 
   const todoList = [];
@@ -61,4 +61,18 @@ const TodoPrivateList = props => {
   );
 };
 
-export default TodoPrivateList;
+const TodoPrivateListQuery = () => {
+  const {loading, error, data} = useQuery(GET_MY_TODOS);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    console.error(error);
+    return <div>Error!!!</div>
+  }
+  return <TodoPrivateList todos={data.todos} />
+}
+
+export default TodoPrivateListQuery;
+export { GET_MY_TODOS }
